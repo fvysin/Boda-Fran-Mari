b3d9d55a03935f82a7882d675d92f934503de397
fvysin <florvysin@gmail.com>
2025-01-25 11:55:10 +0100

    retoques

--- src/Components/Tips.jsx
+++ src/Components/Tips.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useRef } from 'react';
 import { useOnScreen } from '../Components/useOnScreen';
 import './Tips.css'
@@ -9,15 +10,9 @@ export const Tips = () => {
   return (
     <div className={`container-tips ${isVisible ? 'show' : ''}`} ref={ref}>
          <h1 className='titulotraslados'>Fiesta</h1>
-         {/* <lord-icon
-           src="https://cdn.lordicon.com/ohcuigqh.json"
-           trigger="loop"
-          colors="primary:#000000,secondary:#9fddea"
-          style={{ width: '50px', height: '50px' }}>
-        </lord-icon> */}
         <div className='texto-tips'>
          <p >Detalles a tener en cuenta:</p>
-         <p>Para estas esceneas necesitaremos un <span>DRESS CODE FORMAL</span></p>
+         <p>Para estas esceneas necesitaremos un <br /><span className='drescode-span'>Dress code formal</span></p>
         </div>
         <div className='dresscode'>
            <img src={dresscode} alt="dresscode" />
